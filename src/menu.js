9d0c09e92eb0b0a05212e58c3ab5cd7e227f66c3
Douglas Senzu <senzdigitals.inc23@gmail.com>
2025-06-21 18:30:23 +0000

    Completed design of menu page

--- src/menu.js
+++ src/menu.js
@@ -1,15 +1,69 @@
 
-import { createElements, attachElements } from "./create-elements";
+import { createElements, attachElements, setAttributes } from "./create-elements";
+
+import menupic from "./assets/images/restaurant-menu-pic.png";
 
 const menucontainer = (() => {
     let menucontainer = createElements('div', 'menuinner');
 
     return menucontainer;
-})()
+})();
+
+const menuImage = (() => {
+    let img = createElements('img', 'menuimage');
+
+    setAttributes({'src': menupic}, img)
+    return img;
+})();
+
+const menuItems = (() => {
+    let menuItemsContainer = createElements('div', 'menuitemscont');
+
+    return menuItemsContainer;
+})();
+
+let itemOne = (() => {
+    let itemsdiv = createElements('div', 'itemsdiv');
+    let menu = createElements('div', 'menu', 'MENU')
+    let itemTitle = createElements('div', 'itemtitle', 'Local Dishes');
+    let riceAndStew = createElements('div', 'riceandstew', '1. Rice & Stew - Ghs35.00');
+    let fufuAndLightSoup = createElements('div', 'fufulightsoup', '2. Fufu With Light Soup - Ghs50.00');
+    let fufuAndPalmnutSoup = createElements('div', 'fufupalmsoup', '3. Fufu With Palmnut Soup - Ghs50.00');
+    let fufuAndGroundnutSoup = createElements('div', 'fufugroundnutsoup', '4. Fufu With Light Soup - Ghs50.00')
+
+
+    attachElements(itemsdiv, [
+        menu, itemTitle, riceAndStew, fufuAndLightSoup,
+        fufuAndPalmnutSoup, fufuAndGroundnutSoup
+    ])
+
+    return itemsdiv;
+
+})();
+
+let itemTwo = (() => {
+    let itemsdiv = createElements('div', 'itemsdiv');
+    let itemTitle = createElements('div', 'itemtitle', 'Continental Dishes');
+    let riceAndStew = createElements('div', 'riceandstew', '1. Baicon Burgar - Ghs98.00');
+    let fufuAndLightSoup = createElements('div', 'springrolls', '2. East West Spring Rolls - Ghs60.00');
+    let fufuAndPalmnutSoup = createElements('div', 'paneersteak', '3. Paneer Steak - Ghs60.00');
+    let fufuAndGroundnutSoup = createElements('div', 'roastsalad', '4. Roast Lamb Salad - Ghs50.00')
+    let roastedturkey = createElements('div', 'roastedturkey', '4. Roast Turkey With Cranberry Sauce - Ghs50.00')
+
+    attachElements(itemsdiv, [
+        itemTitle, riceAndStew, fufuAndLightSoup,
+        fufuAndPalmnutSoup, roastedturkey
+    ])
+
+    return itemsdiv;
+
+})();
 
 export const  menu = (() => {
     let menu = createElements('div', 'menudiv');
 
+    attachElements(menuItems, [itemOne, itemTwo])
+    attachElements(menucontainer, [menuImage, menuItems])
     attachElements(menu, [menucontainer])
 
 
